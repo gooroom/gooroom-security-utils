8032238f7cda859424053e0614b10e2c81a6d9d1
hmkim <hmkim@gooroom.kr>
2017-08-18 16:05:13 +0900

    update logparser for agent-tray-icon, remove python3-dev in control of Depends
    Change-Id: I9e4007ce0863bcde11e22139de2d24cf2edffaa5

--- data/gooroom-security-logparser.py
+++ data/gooroom-security-logparser.py
@@ -10,6 +10,7 @@ from systemd import journal
 
 #-----------------------------------------------------------------------
 SECURITY_FEATURES = ('os', 'exe', 'boot', 'media')
+STATUS_KOR_TO_ENG = {'안전':'safety', '취약':'vulnerable', '중단':'stop'}
 
 #-----------------------------------------------------------------------
 def get_summary():
@@ -17,10 +18,6 @@ def get_summary():
     보안기능(os,exe,boot,media)의 journal로그를 파싱해서
     요약로그정보를 출력
     """
-    normal_mode = True
-
-    if len(sys.argv) > 1 and sys.argv[1].lower() == 'emul':
-        normal_mode = False
 
     logs = []
     func_name = None
@@ -32,25 +29,23 @@ def get_summary():
         'SYSLOG_IDENTIFIER=gop-daemon', 'SYSLOG_IDENTIFIER=grac-daemon',
         'PRIORITY=3', '_TRANSPORT=audit']
 
-    if normal_mode:
-        j = journal.Reader()
+    j = journal.Reader()
 
-        # 시간과 필터 설정
-        #from_date_time = datetime.datetime.strptime("2017-08-11", "%Y-%m-%d")
-		#j.seek_realtime(from_date_time)
-        for match in match_strings:
-            j.add_match(match)
-            j.add_disjunction()
+    # 시간과 필터 설정
+    if len(sys.argv) > 1:
+        from_time = datetime.datetime.strptime(sys.argv[1], '%Y%m%d-%H%M%S.%f')
+        j.seek_realtime(from_time)
 
-        # 엔트리를 추출해서 로그 배열에 별도 저장
-        for entry in j:
-            if type(entry['MESSAGE']) is bytes:
-                entry['MESSAGE'] = str(entry['MESSAGE'].decode('unicode_escape').encode('utf-8'))
-            logs.append(entry)
+    for match in match_strings:
+        j.add_match(match)
+        j.add_disjunction()
 
-        func_name = 'get_summary'
-    else:
-        func_name = 'get_summary_emul'
+    # 엔트리를 추출해서 로그 배열에 별도 저장
+    for entry in j:
+        if type(entry['MESSAGE']) is bytes:
+            entry['MESSAGE'] = \
+                str(entry['MESSAGE'].decode('unicode_escape').encode('utf-8'))
+        logs.append(entry)
 
     result = {}
     status_summary = "안전"
@@ -59,18 +54,23 @@ def get_summary():
 
     for sf in SECURITY_FEATURES:
         m = importlib.import_module('security.'+sf)
-        status, log = getattr(m, func_name)(logs)
+        status, log = getattr(m, 'get_summary')(logs)
 
-        result[sf+'_status'] = status
+        result[sf+'_status'] = STATUS_KOR_TO_ENG[status]
         result[sf+'_log'] = log
 
         if status == '취약':
-            status_summary = status
+            status_summary = '취약'
 
-    result['status_summary'] = status_summary
+    result['status_summary'] = STATUS_KOR_TO_ENG[status_summary]
     return result
 
 if __name__ == '__main__':
 
-	print(json.dumps(get_summary(), ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': ')))
+    print('JSON-ANCHOR=%s' % json.dumps(
+                                get_summary(), 
+                                ensure_ascii=False, 
+                                sort_keys=True, 
+                                indent=4, 
+                                separators=(',', ': ')))
 
