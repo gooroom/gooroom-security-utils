7736696c221c241b8c66ec95be4680490ca80613
hmkim <hmkim@gooroom.kr>
2018-10-29 11:37:16 +0900

    changed to new log system
    Change-Id: I4a0f2a9907e1d1dcf35c57a47065cae254d3382e

--- data/gooroom-security-logparser.py
+++ data/gooroom-security-logparser.py
@@ -1,35 +1,258 @@
 #! /usr/bin/env python3
 
 #-----------------------------------------------------------------------
+import sys
+sys.path.append('/usr/lib/gooroom-security-utils')
+
 import simplejson as json
 import importlib
-import sys
 import datetime
+import re
 
+from gsl_util import combine_message,JournalLevel,status_lang_set
+from gsl_util import get_run_status,format_exc,load_log_config
+from gsl_util import load_translation,syslog_identifier_map
 from systemd import journal
 
 #-----------------------------------------------------------------------
-SECURITY_FEATURES = ('os', 'exe', 'boot', 'media')
-STATUS_KOR_TO_ENG = {'안전':'safe', '취약':'vulnerable', '동작': 'run', '중단':'stop'}
+g_trans_parser = load_translation()
 
 #-----------------------------------------------------------------------
-def get_summary():
+#GRAC is using this function
+#Do not move
+def get_notify_level(printname):
+    """
+    get notify-level in config
+    """
+
+    return JournalLevel[load_log_config()[printname]['notify_level']].value
+
+#-----------------------------------------------------------------------
+def identifier_processing(entry, mode, printname, notify_level, result):
+    """
+    SYSLOG_IDENTIFIER가 있는 로그를 처리
+    """
+
+    #GRMCODE
+    if 'GRMCODE' in entry:
+        grmcode = entry['GRMCODE']
+    else:
+        grmcode = ''
+
+    #MESSAGE
+    if 'MESSAGE' in entry:
+        message = entry['MESSAGE']
+
+        if type(message) is bytes:
+            message = str(message.decode('unicode_escape').encode('utf-8'))
+    else:
+        message = ''
+
+    #TRANSLATION
+    if grmcode:
+        try:
+            ko = g_trans_parser.get(grmcode, 'ko')    
+            message = combine_message(message, ko)
+        except:
+            pass
+
+    #SAVE
+    t = entry['__REALTIME_TIMESTAMP'].strftime('%Y-%m-%d %H:%M:%S.%f')
+    log = {
+        'grmcode':grmcode,
+        'level':JournalLevel(entry['PRIORITY']).name,
+        'time':t,
+        'log':'{} {}'.format(t, message),
+        'type':status_lang_set(mode, 0) }
+
+    result[printname+'_log'].append(log)
+
+    if entry['PRIORITY'] <= JournalLevel[notify_level].value:
+        result[printname+'_status'] = status_lang_set(mode, 'vulnerable')
+        result['status_summary'] = status_lang_set(mode, 'vulnerable')
+        log['type'] = status_lang_set(mode, 1)
+
+    return len(log['log'])
+
+#-----------------------------------------------------------------------
+GRAC_NETWORK_NAME = 'GRAC: Disallowed Network'
+P_SRC_IP = re.compile('SRC=\S*')
+P_DST_IP = re.compile('DST=\S*')
+P_SRC_PORT = re.compile('SPT=\S*')
+P_DST_PORT = re.compile('DPT=\S*')
+
+P_CAUSE = re.compile('cause=\S*')
+P_FILE = re.compile('name=\S*')
+P_COMM = re.compile('comm=\S*')
+
+KOREAN_TEXT = {
+    u'"invalid-hash"' : u'비정상 해시',
+    u'"invalid-signature"' : u'비정상 서명',
+    u'"missing-hash"' : u'서명 미존재',
+    u'"no_label"' : '레이블 미존재',
+    u'"IMA-signature-required"' : u'서명 미존재'}
+
+def no_identifier_processing(entry, mode, result):
+    """
+    SYSLOG_IDENTIFIER가 없는 로그를 처리
+    :gep,iptables
+    """
+
+    message = ''
+    printname = ''
+
+    # iptables
+    if 'kernel' in entry['_TRANSPORT'] and GRAC_NETWORK_NAME in data['MESSAGE']:
+        search_src_ip = P_SRC_IP.search(data['MESSAGE'])
+        search_dst_ip = P_DST_IP.search(data['MESSAGE'])
+        search_src_port = P_SRC_PORT.search(data['MESSAGE'])
+        search_dst_port = P_DST_PORT.search(data['MESSAGE'])
+
+        if (search_src_ip == None or search_src_port == None or
+            search_dst_ip == None or search_dst_port == None):
+            return 0
+
+        src_ip_string = search_src_ip.group().replace('SRC=', '')
+        src_port_string = search_src_port.group().replace('SPT=', '')
+        dst_ip_string = search_dst_ip.group().replace('DST=', '')
+        dst_port_string = search_dst_port.group().replace('DPT=', '')
+
+        t = entry['__REALTIME_TIMESTAMP'].strftime('%Y-%m-%d %H:%M:%S.%f')
+        message = \
+            '비인가 네트워크 패킷(출발지 {}:{}, 목적지 {}:{})이'\
+            '탐지되어 차단하였습니다'.format(
+                                        src_ip_string, 
+                                        src_port_string, 
+                                        dst_ip_string, 
+                                        dst_port_string)
+        printname = 'media'
+
+	# 커널의 IMA에서 남긴 로그
+    elif 'audit' in entry['_TRANSPORT']:
+        search_cause = P_CAUSE.search(data['MESSAGE'])
+        search_file = P_FILE.search(data['MESSAGE'])
+        search_comm = P_COMM.search(data['MESSAGE'])
+
+        if search_cause != None and search_file != None:
+            cause_string = search_cause.group().replace('cause=', '')
+            if '"' in search_file.group():
+                file_string = \
+                    search_file.group().replace('name=', '').replace('"', '')
+            else:
+                file_string = \
+                    search_comm.group().replace('comm=', '').replace('"', '')
+            # 시간 변환 및 메시지 한글화
+            cause_string = KOREAN_TEXT[cause_string]
+
+            # no_label의 경우, name 필드에 해시 값이 들어있으므로
+            #사람이 인지할 수 있는 파일명으로 변환
+            if cause_string == '"no_label"':
+                file_string = data['_AUDIT_FIELD_NAME'].replace('"', '')
+
+            # 로그 추가
+            message = '비인가된 실행파일(%s, %s)이'\
+                '실행되어 차단하였습니다'.format(cause_string, file_string)
+            printname = 'gep-daemon'
+
+    if message:
+        t = entry['__REALTIME_TIMESTAMP'].strftime('%Y-%m-%d %H:%M:%S.%f')
+        log = {
+            'grmcode':'',
+            'level':JournalLevel(entry['PRIORITY']).name,
+            'time':t,
+            'log':'{} {}'.format(t, message),
+            'type':status_lang_set(1) }
+
+        result[printname+'_log'].append(log)
+        result[printname+'_status'] = status_lang_set(mode, 'vulnerable')
+        result['status_summary'] = status_lang_set(mode, 'vulnerable')
+        return len(log['log'])
+    else:
+        return 0
+
+#-----------------------------------------------------------------------
+def get_summary(j, mode='DAEMON'):
     """
     보안기능(os,exe,boot,media)의 journal로그를 파싱해서
     요약로그정보를 출력
     """
 
-    func_name = None
-    """
-    SYSLOG_IDENTIFIER는 데몬의 로그를 수집하기 위함이며, PRIORITY와 _TRANSPORT는
-    각각 OS 보호 기술 및 실행파일 보호 기술의 로그를 수집하기 위함이다.
-    """
-    match_or_strings = ['SYSLOG_IDENTIFIER=gbp-daemon', 'SYSLOG_IDENTIFIER=gep-daemon',
-        'SYSLOG_IDENTIFIER=gop-daemon', 'SYSLOG_IDENTIFIER=GRAC',
-        'SYSLOG_IDENTIFIER=gooroom-browser', '_AUDIT_FIELD_OP="appraise_data"']
+    if mode == 'DAEMON':
+        level_key = 'transmit_level'
+    else:
+        level_key = 'show_level'
 
-    # OS 보호 및 방화벽 로그를 추려내기위한 특수한 필터
-    match_and_strings = ['PRIORITY=3', 'SYSLOG_IDENTIFIER=kernel']
+    log_json = load_log_config()
+    identifier_map = syslog_identifier_map(log_json)
+
+    #identifier and priority
+    for identifier, printname in identifier_map.items():
+        match_identifier = 'SYSLOG_IDENTIFIER='+identifier
+        j.add_match(match_identifier)
+
+        value = JournalLevel[log_json[printname][level_key]].value
+        for i in range(value+1):
+            match_priority = \
+                'PRIORITY={}'.format(i)
+            j.add_match(match_priority)
+        j.add_disjunction()
+
+    #IMA(gep)
+    j.add_match('_AUDIT_FIELD_OP=appraise_data')
+    j.add_match('PRIORITY=3')
+    j.add_disjunction()
+
+    #OS 보호 및 방화벽 로그를 추려내기위한 특수한 필터
+    j.add_match('SYSLOG_IDENTIFIER=kernel')
+    j.add_match('PRIORITY=3')
+
+    #초기화 및 실행상태, 로그등급
+    result = {}
+    for identifier, printname in identifier_map.items():
+        if 'service_name' in log_json[printname]:
+            service_name = log_json[printname]['service_name']
+        else:
+            service_name = ''
+        run = get_run_status(service_name)
+
+        result[printname+'_run'] = status_lang_set(mode, run)
+        result[printname+'_status'] = status_lang_set(mode, 'safe')
+        result[printname+'_notify_level'] = log_json[printname]['notify_level']
+        result[printname+'_show_level'] = log_json[printname][level_key]
+        result[printname+'_log'] = []
+    result['status_summary'] = status_lang_set(mode, 'safe')
+
+    log_total_len = 0
+
+    for entry in j:
+        if '_KERNEL_SUBSYSTEM' in entry.keys():
+            continue
+
+        try:
+            #gbp,gop,grac,browser와 같이 SYSLOG_IDENTIFIER를 사용하는 경우와
+            #gep,iptables와 같이 별도로 처리해야 되는 경우를 구분
+            if 'SYSLOG_IDENTIFIER' in entry \
+                and entry['SYSLOG_IDENTIFIER'] in identifier_map.keys():
+
+                printname = identifier_map[entry['SYSLOG_IDENTIFIER']]
+                notify_level = log_json[printname]['notify_level']
+                log_total_len += \
+                    identifier_processing(entry, 
+                                        mode, 
+                                        printname, 
+                                        notify_level, 
+                                        result)
+            else:
+                log_total_len += \
+                    no_identifier_processing(entry, mode, result)
+        except:
+            print(format_exc())
+
+    result['log_total_len'] = log_total_len
+    return result
+
+#-----------------------------------------------------------------------
+if __name__ == '__main__':
 
     j = journal.Reader()
 
@@ -38,49 +261,11 @@ def get_summary():
         from_time = datetime.datetime.strptime(sys.argv[1], '%Y%m%d-%H%M%S.%f')
         j.seek_realtime(from_time)
 
-    for match in match_or_strings:
-        j.add_match(match)
-        j.add_disjunction()
-
-    for match in match_and_strings:
-        j.add_match(match)
-
-    logs = []
-    # 엔트리를 추출해서 로그 배열에 별도 저장
-    for entry in j:
-        if '_KERNEL_SUBSYSTEM' in entry.keys():
-            continue
-
-        if type(entry['MESSAGE']) is bytes:
-            entry['MESSAGE'] = \
-                str(entry['MESSAGE'].decode('unicode_escape').encode('utf-8'))
-
-        logs.append(entry)
-
-    result = {}
-    status_summary = "안전"
-
-    sys.path.append('/usr/lib/gooroom-security-utils')
-
-    for sf in SECURITY_FEATURES:
-        m = importlib.import_module('security.'+sf)
-        run, status, log = getattr(m, 'get_summary')(logs)
-
-        result[sf+'_run'] = STATUS_KOR_TO_ENG[run]
-        result[sf+'_status'] = STATUS_KOR_TO_ENG[status]
-        result[sf+'_log'] = log
-
-        if status == '취약':
-            status_summary = '취약'
-
-    result['status_summary'] = STATUS_KOR_TO_ENG[status_summary]
-    return result
-
-if __name__ == '__main__':
-
     print('JSON-ANCHOR=%s' % json.dumps(
-                                get_summary(), 
+                                get_summary(j, mode='GUI'), 
                                 ensure_ascii=False, 
                                 sort_keys=True, 
                                 indent=4, 
                                 separators=(',', ': ')))
+
+
---
+++ data/gsl_util.py
@@ -0,0 +1,175 @@
+#! /usr/bin/env python3
+
+#-----------------------------------------------------------------------
+import simplejson as json
+import configparser
+import subprocess
+import traceback
+import shlex
+import re
+
+#-----------------------------------------------------------------------
+DEFAULT_LOG_CONF_PATH=\
+    '/usr/lib/gooroom-security-utils/default-log.conf'
+LOG_CONF_PATH=\
+    '/usr/lib/gooroom-security-utils/log.conf'
+LOG_CONF_SIGN_PATH=\
+    '/usr/lib/gooroom-security-utils/log.conf.sign'
+TRANSLATION_PATH=\
+    '/usr/lib/gooroom-security-utils/translation'
+
+#-----------------------------------------------------------------------
+import enum
+class JournalLevel(enum.Enum):
+    emerg =     0
+    alert =     1
+    crit =      2
+    err =       3
+    warning =   4
+    notice =    5
+    info =      6
+    debug =     7
+
+#-----------------------------------------------------------------------
+STATUS_LANG_SET = { 
+    'safe':'안전', 'vulnerable':'취약', 'run':'동작', 
+    'stop':'중단', 1:'취약', 0:'안전' }
+
+def status_lang_set(mode, lang):
+    """
+    상태 문자 번역판
+    """
+
+    if mode == 'DAEMON':
+        return STATUS_LANG_SET[lang]
+    else:
+        return lang
+
+#-----------------------------------------------------------------------
+def get_run_status(service_name):
+    """
+    로그와 시스템의 특정 정보를 이용해서 
+    안전하게 부팅되었는지 여부를 반환한다.
+    """
+
+    if not service_name:
+        return 'stop'
+
+    # 서비스 구동 상태와 정상 설치 여부를 검사
+    cmd = '/usr/sbin/service {} status'.format(service_name)
+    argv = shlex.split(cmd)
+    pipe = subprocess.Popen(
+        argv, 
+        stdout=subprocess.PIPE, 
+        stderr=None, 
+        shell=False)
+    status_output, error =  pipe.communicate()
+    status_output = status_output.decode('utf-8')
+
+    cmd = '/usr/sbin/service {} check'.format(service_name)
+    argv = shlex.split(cmd)
+    pipe = subprocess.Popen(
+        argv, 
+        stdout=subprocess.PIPE, 
+        stderr=None, 
+        shell=False)
+    check_output, error =  pipe.communicate()
+    check_output = check_output.decode('utf-8')
+
+    if not 'active (exited)' in status_output \
+        and not 'active (running)' in status_output:
+        return 'stop'
+
+    if service_name != 'grac-device-daemon' \
+        and service_name != 'gooroom-agent' \
+        and not '{} active.'.format(service_name) in check_output:
+            return 'stop'
+    return 'run'
+
+#-----------------------------------------------------------------------
+def format_exc():
+    """
+    reprlib version of format_exc of traceback
+    """
+
+    return '\n'.join(traceback.format_exc().split('\n')[-4:-1])
+
+#-----------------------------------------------------------------------
+def load_log_config():
+    """
+    서버설정파일의 로딩에 실패하면
+    기본설정파일을 반환.
+    기본설정파일의 로딩에 실패하면
+    {}을 반환.
+    """
+    
+    try:
+        with open(LOG_CONF_PATH) as f2:
+            log_string = f2.read()
+        with open(LOG_CONF_SIGN_PATH) as f3:
+            log_sign = f3.read()
+
+        verify_signature(log_sign, log_string)
+        return json.loads(log_string)
+    except:
+        pass
+        
+    try:
+        with open(DEFAULT_LOG_CONF_PATH) as f:
+            return json.loads(f.read())
+    except:
+        print(format_exc())
+
+    return {}
+
+#-----------------------------------------------------------------------
+def load_translation():
+    """
+    번역파일을 로딩
+    """
+
+    parser = configparser.RawConfigParser()
+    parser.optionxform = str
+    parser.read(TRANSLATION_PATH)
+    return parser
+
+#-----------------------------------------------------------------------
+def combine_message(message, ko):
+    """
+    메세지 안에 있는 토큰을 분리해서 번역문을 완성한다
+    """
+
+    try:
+        token_regix = re.compile('\$\(\w+\)')
+
+        tokens = token_regix.findall(message)
+        msg_values = []
+        for token in tokens:
+            msg_values.append(token[2:-1])
+
+        ko_regix = re.compile('\$\(\d+\)')
+        holders = ko_regix.findall(ko)
+        for holder in holders:
+            idx = int(holder[2:-1])
+            ko = ko.replace(holder, msg_values[idx])
+
+        return ko
+    except:
+        print(format_exc())
+        print('failed message===>{}\t{}'.format(message, ko))
+        raise
+                
+#-----------------------------------------------------------------------
+def syslog_identifier_map(log_json):
+    """
+    로그설정에서 syslog_identifier 리스트를 반환
+    """
+
+    d = {}
+    for printname, infos in log_json.items():
+        identifiers = [ifs.strip() 
+                        for ifs in infos['syslog_identifier'].split(',')]
+        for identifier in identifiers:
+            d[identifier] = printname
+    return d
+
